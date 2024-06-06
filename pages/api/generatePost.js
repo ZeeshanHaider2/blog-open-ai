d70942840cd47c97439f5d713391045d5be38d9a
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-06-06 20:20:28 +0200

    changes in generate post to fix metadescription and title insertion in mongodb

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,12 +1,11 @@
 import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
-import { OpenAIApi, Configuration } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import clientPromise from "../../lib/mongodb";
 
 export default withApiAuthRequired(async function handler(req, res) {
   const { user } = await getSession(req, res);
   const client = await clientPromise;
   const db = client.db("BlogStandard");
-
   const userProfile = await db.collection("users").findOne({
     auth0Id: user.sub,
   });
@@ -23,81 +22,130 @@ export default withApiAuthRequired(async function handler(req, res) {
 
   const { topic, keywords } = req.body;
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-1106",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are an SEO friendly blog post generator called blog standard. You are designed to output markdown without frontmatter",
-      },
-      {
-        role: "user",
-        content: `
-        Generate a Blog post on topic delimited by ---hyphens:
-        ${topic}
-         ---
-         Targeting the following comma separated keywords delimited by triple hyphens:
-         ---
-         ${keywords}
-         ---
-        `,
-      },
-    ],
-  });
+  if (!topic || !keywords) {
+    res.status(422);
+    return;
+  }
 
-  //console.log(response.data.choices[0]?.message?.content);
-  const postContent = response.data.choices[0]?.message?.content;
+  if (topic.length > 80 || keywords.length > 80) {
+    res.status(422);
+    return;
+  }
 
-  const seoResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo-1106",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are an SEO friendly blog post generator called blog standard. You are designed to output JSON. Do not include HTML tags in your output",
-      },
-      {
-        role: "user",
-        content: `
-        Generate an SEO friendly title and SEO friendly meta description for the following blog post.
-        ${postContent}
-        ---
-        The output JSON must be in the following format:
-      {
-        "title":"example title",
-        "metaDescription":"example meta description"
-      }`,
-      },
-    ],
-    response_format: { type: "json_object" },
-  });
-  console.log(seoResponse.data.choices[0]?.message?.content);
-  const { title, metaDescription } =
-    seoResponse.data.choices[0]?.message?.content || {};
-  //console.log(seoResponse.data.choices[0]?.message?.content);
-
-  await db.collection("users").updateOne(
+  /*const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    temperature: 0,
+    max_tokens: 3600,
+    prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}.
+    The content should be formatted in SEO-friendly HTML.
+    The response must also include appropriate HTML title and meta description content.
+    The return format must be stringified JSON in the following format:
     {
-      auth0Id: user.sub,
+      "postContent": post content here
+      "title": title goes here
+      "metaDescription": meta description goes here
+    }`,
+  });*/
+
+  const postContentResult = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a blog post generator.",
+      },
+      {
+        role: "user",
+        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+      },
+    ],
+    temperature: 0,
+  });
+
+  const postContent = postContentResult.data.choices[0]?.message.content;
+
+  const titleResult = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a blog post generator.",
+      },
+      {
+        role: "user",
+        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+      },
+      {
+        role: "assistant",
+        content: postContent,
+      },
+      {
+        role: "user",
+        content: "Generate appropriate title tag text for the above blog post",
+      },
+    ],
+    temperature: 0,
+  });
+
+  const metaDescriptionResult = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a blog post generator.",
+      },
+      {
+        role: "user",
+        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+      },
+      {
+        role: "assistant",
+        content: postContent,
+      },
+      {
+        role: "user",
+        content:
+          "Generate SEO-friendly meta description content for the above blog post",
+      },
+    ],
+    temperature: 0,
+  });
+
+  const title = titleResult.data.choices[0]?.message.content;
+  const metaDescription =
+    metaDescriptionResult.data.choices[0]?.message.content;
+
+  console.log("POST CONTENT: ", postContent);
+  console.log("TITLE: ", title);
+  console.log("META DESCRIPTION: ", metaDescription);
+
+  /*await db.collection('users').updateOne(
+  {
+    auth0Id: user.sub,
+  },
+  {
+    $inc: {
+      availableTokens: -1,
     },
-    {
-      $inc: {
-        availableTokens: -1,
-      },
-    }
-  );
+  }
+);*/
 
   const post = await db.collection("posts").insertOne({
-    postContent,
-    title,
-    metaDescription,
+    postContent: postContent || "",
+    title: title || "",
+    metaDescription: metaDescription || "",
     topic,
     keywords,
     userId: userProfile._id,
     created: new Date(),
   });
-  console.log("POST:", post);
+
   res.status(200).json({
     postId: post.insertedId,
   });
