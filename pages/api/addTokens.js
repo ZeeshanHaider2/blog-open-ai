c01f797035e2d8578717eefd9c76190cd9665949
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-06-07 18:35:50 +0200

    stripe setup

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,11 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getSession } from "@auth0/nextjs-auth0";
 import clientPromise from "../../lib/mongodb";
+import stripeInit from "stripe";
+
+const stripe = stripeInit(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   const { user } = await getSession(req, res);
 
-  //console.log("user:", user);
+  const lineItems = [
+    {
+      price: process.env.STRIPE_PRODUCT_PRICE_ID,
+      quantity: 1,
+    },
+  ];
+
+  const protocol =
+    process.env.NODE_ENV === "development" ? "http://" : "https://";
+  const host = req.headers.host;
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: `${protocol}${host}/success`,
+  });
   const client = await clientPromise;
   const db = client.db("BlogStandard");
 
@@ -26,5 +44,5 @@ export default async function handler(req, res) {
     }
   );
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ session: checkoutSession });
 }
